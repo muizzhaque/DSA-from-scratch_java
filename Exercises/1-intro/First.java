0ab95318fb02c19df0c42a525791faa3fc6c53f7
muizz-IHT <muizz@intelligenthealthcare.com>
2024-03-16 14:58:18 +0530

    added

---
+++ Exercises/1-intro/First.java
@@ -0,0 +1,24 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Scanner;
+// import org.junit.jupiter.api.Test;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        try (Scanner input = new Scanner(System.in)) {
+      // System.out.println(input.nextInt());
+        System.out.println(input.next());
+        System.out.println(input.nextLine());
+    }
+}
+
+// public class Demo {
+//   public static void main(String[] args) {
+//       System.out.println(args[0]);
+//     }
+// }
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
